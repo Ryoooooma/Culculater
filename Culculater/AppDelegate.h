a356a7202257bc4cc3f0933c0c928717a0c236a6
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-06 12:14:42 +0800

    Initial Commit

---
+++ Culculater/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Culculater
+//
+//  Created by 脇田竜馬 on 2015/08/06.
+//  Copyright (c) 2015年 Ryoma Wakita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Culculater/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Culculater
+//
+//  Created by 脇田竜馬 on 2015/08/06.
+//  Copyright (c) 2015年 Ryoma Wakita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
